ca53417b731f4325400852c5a28374460aac8569
Oizopower <oizopower@whitecoin.info>
2017-11-07 22:37:22 +0100

    Add Chinese translation

---
+++ Languages/ch_CH.php
@@ -0,0 +1,86 @@
+<?php
+
+$language = array(
+
+    // Topmenu
+    "staking-status"                                                        => "利息状态",
+    "wallet-status"                                                         => "钱包状态",
+    "connections"                                                           => "联系",
+    "settings"                                                              => "设置",
+    "logout"                                                                => "退出",
+
+    // Menu
+    "staking-info"                                                          => "利息信息",
+    "transactions"                                                          => "交易",
+    "send"                                                                  => "发送",
+    "receive"                                                               => "接收",
+    "wallet-info"                                                           => "钱包信息",
+    "encrypt-your-wallet"                                                   => "加密你的钱包",
+    "encrypt-wallet"                                                        => "加密钱包",
+    "unlock-wallet-for-staking"                                             => "解密钱包收利息",
+    "unlock-for-staking"                                                    => "为收利息解锁",
+    "download-wallet"                                                       => "下载钱包",
+    "system"                                                                => "系统",
+    "reboot"                                                                => "重启",
+    "shutdown"                                                              => "关机",
+    "update-gui"                                                            => "更新GUI",
+
+
+    // Footer
+    "whitecoin"                                                             => "百特币",
+    "website"                                                               => "网站",
+    "explorer"                                                              => "浏览器",
+    "reddit"                                                                => "Reddit",
+    "facebook"                                                              => "Facebook",
+    "twitter"                                                               => "推特",
+
+    // Settings
+    "apply-custom-settings-here"                                            => "使用用户的设置",
+    "enable-login"                                                          => "允许登录",
+    "username"                                                              => "用户名",
+    "password"                                                              => "密码",
+    "block-explorer"                                                        => "区块浏览器",
+    "coin-marketcap"                                                        => "市场交易量",
+    "save-settings"                                                         => "保存设置",
+
+    // Dashboard
+    "dashboard"                                                             => "常用信息",
+    "synchronizing"                                                         => "同步",
+    "staking"                                                               => "正在收利息",
+    "last-price"                                                            => "最后的价格",
+    "last-updated"                                                          => "最后更新",
+    "revenue"                                                               => "收入",
+    "bittrex---btc-price"                                                   => "Bittrex-BTC价格",
+    "blocks"                                                                => "区块",
+    "total-blocks-since"                                                    => "区块总数",
+    "balance"                                                               => "余额",
+    "recent-transactions"                                                   => "最近的交易",
+    "view-your-recent-transactions-below"                                   => "查看以下最近的交易",
+    "date"                                                                  => "日期",
+    "account"                                                               => "帐户",
+    "amount"                                                                => "数额",
+    "confirmations"                                                         => "确认",
+    "category"                                                              => "类别",
+
+    // Staking Info
+    "network-stake-weight"                                                  => "网络权重",
+    "your-weight"                                                           => "你的权重",
+    "network-weight-based-on-the-amount-of-coins"                           => "基于百特币数量的网络权重",
+    "expected-stake"                                                        => "预期权重",
+    "latest-stake"                                                          => "最新利息",
+    "reserved-coins-unable-to-spend"                                        => "保留硬币",
+    "stake-calculator"                                                      => "利息计算器",
+    "calcultated-from-current"                                              => "从目前的网络重量和平衡计算一年后。目前的百分比是",
+    "staking-coins"                                                         => "正在收利息的币",
+    "compound-interest"                                                     => "复利",
+    "xwc"                                                                   => "百特币",
+    "recent-stakes"                                                         => "最近的利息",
+    "view-your-recent-stakes-below"                                         => "浏览你最近的利息",
+
+
+    // misc (might be deleted soon
+    "page-being-developed"                                                  => "正在开发的页面",
+    "use-command-line-for-now"                                              => "现在使用命令行",
+    "but-here-is-the-receiving-address"                                     => "这是接收地址",
+
+);
