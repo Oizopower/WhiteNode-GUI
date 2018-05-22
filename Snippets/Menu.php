f8e8a47635ae339d1c9e331dda9972f73ffe75a5
Oizopower <oizopower@whitecoin.info>
2018-05-22 21:55:27 +0200

    Remove submenu functions for later

--- Snippets/Menu.php
+++ Snippets/Menu.php
@@ -1,66 +1,58 @@
 <ul class="nav">
     <?= $pageData['menu']; ?>
-
-    <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="ti-package"></i>
-            <p><?= tl("System") ?>
-                <b class="caret"></b>
-            </p></a>
-        <ul class="dropdown-menu">
-            <li>
-                <a href="/webconsole.php" target="_blank">
-                    <i class="fa fa-desktop"></i>
-                    <p><?= tl("Terminal") ?></p>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="js--doAction" data-action="reboot" data-title="Reboot" data-content="Are you sure you want to reboot?">
-                    <i class="fa fa-refresh icon-warning"></i>
-                    <p><?= tl("Reboot") ?></p>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="js--doAction" data-action="shutdown" data-title="Shutdown" data-content="Are you sure you want to shutdown?">
-                    <i class="fa fa-power-off icon-danger"></i>
-                    <p><?= tl("Shutdown") ?></p>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="js--doAction" data-action="updateGUI" data-title="Update" data-content="Are you sure you want to update the GUI?">
-                    <i class="fa fa-cog icon-success"></i>
-                    <p><?= tl("Update GUI") ?></p>
-                </a>
-            </li>
-            <?php
-            if (Wallet::isWalletRunning())
+        <li>
+            <a href="/webconsole.php" target="_blank">
+                <i class="fa fa-desktop"></i>
+                <p><?= tl("Terminal") ?></p>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="js--doAction" data-action="reboot" data-title="Reboot" data-content="Are you sure you want to reboot?">
+                <i class="fa fa-refresh icon-warning"></i>
+                <p><?= tl("Reboot") ?></p>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="js--doAction" data-action="shutdown" data-title="Shutdown" data-content="Are you sure you want to shutdown?">
+                <i class="fa fa-power-off icon-danger"></i>
+                <p><?= tl("Shutdown") ?></p>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="js--doAction" data-action="updateGUI" data-title="Update" data-content="Are you sure you want to update the GUI?">
+                <i class="fa fa-cog icon-success"></i>
+                <p><?= tl("Update GUI") ?></p>
+            </a>
+        </li>
+        <?php
+        if (Wallet::isWalletRunning())
+        {
+            if (!Wallet::isWalletEncrypted())
             {
-                if (!Wallet::isWalletEncrypted())
-                {
-                    ?>
-                    <li class="js--encrypt-button">
-                        <a href="" data-toggle="modal" data-target="#encrypt" data-title="<?= tl("Encrypt your wallet") ?>">
-                            <i class="fa fa-gavel"></i>
-                            <p><?= tl("Encrypt wallet") ?></p>
-                        </a>
-                    </li>
-                    <?php
+                ?>
+                <li class="js--encrypt-button">
+                    <a href="" data-toggle="modal" data-target="#encrypt" data-title="<?= tl("Encrypt your wallet") ?>">
+                        <i class="fa fa-gavel"></i>
+                        <p><?= tl("Encrypt wallet") ?></p>
+                    </a>
+                </li>
+                <?php
 
-                }
-                else if (!Wallet::isWalletStaking())
-                {
-                    ?>
-                    <li class="js--unlock-staking-button">
-                        <a href="" data-toggle="modal" data-target="#stakepassword" data-title="<?= tl("Unlock wallet for staking") ?>">
-                            <i class="fa fa-gavel"></i>
-                            <p><?= tl("Unlock for staking") ?></p>
-                        </a>
-                    </li>
-                    <?php
-                }
             }
-            ?>
-        </ul>
-    </li>
+            else if (!Wallet::isWalletStaking())
+            {
+                ?>
+                <li class="js--unlock-staking-button">
+                    <a href="" data-toggle="modal" data-target="#stakepassword" data-title="<?= tl("Unlock wallet for staking") ?>">
+                        <i class="fa fa-gavel"></i>
+                        <p><?= tl("Unlock for staking") ?></p>
+                    </a>
+                </li>
+                <?php
+            }
+        }
+        ?>
+
 </ul>
 <div class="discord-widget">
     <a href="https://discord.gg/GbpdbMt">
